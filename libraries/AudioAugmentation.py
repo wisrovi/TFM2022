74fb23f375291ac3793aa71e2cb36389049025ff
William Rodriguez <wisrovi.rodriguez@gmail.com>
2022-09-16 17:17:41 -0500

    libreria para incremento de datos a audios

--- libraries/AudioAugmentation.py
+++ libraries/AudioAugmentation.py
@@ -1,6 +1,9 @@
 # https://medium.com/@alibugra/audio-data-augmentation-f26d716eee66
 # https://medium.com/@makcedward/data-augmentation-for-audio-76912b01fdf6
 import self
+from nlpaug.util import AudioVisualizer
+
+from libraries.ProcessAudio import ProcessAudio
 
 try:
     import librosa
@@ -17,18 +20,22 @@ import nlpaug.augmenter.audio as naa
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
-from functools import wraps
 
 
 class AudioAugmentation:
     graph: bool = False
 
     def __init__(self, audio_file, graph=False, save: str = None):
-        self.name_file = audio_file.split(".")[-2].split("/")[-1]
         self.audio_file = audio_file
         self.graph = graph
         self.save = save
-        self.data, self.rate = self.__read_audio_file(self.audio_file)
+        if isinstance(audio_file, str):
+            print("Cargando archivo de audio")
+            self.name_file = audio_file.split(".")[-2].split("/")[-1]
+            self.data, self.rate = self.__read_audio_file(self.audio_file)
+        elif isinstance(audio_file, tuple):
+            print("Audio file is a tuple")
+            self.data, self.rate, self.name_file = audio_file
 
     def middleware_in(atributo1: str = "", ):
         def _middleware_in(f):
@@ -46,11 +53,19 @@ class AudioAugmentation:
                     print(f"Error en la ejecucion de la funcion {f.__name__}: {e}")
 
                 # ejecucion despues de ejecutar la funcion
-                if self.graph:
-                    self.plot_audio(salida)
+                if ejecucion_ok:
+                    if self.graph:
+                        self.plot_audio(salida)
 
-                if self.save is not None:
-                    self.write_audio_file(self.save + self.name_file + "_" + f"{f.__name__}.wav", salida, self.rate)
+                    if self.save is not None:
+                        try:
+                            if isinstance(salida, list):
+                                print(salida)
+                                salida = np.array(salida)
+                            self.write_audio_file(self.save + self.name_file + "_" + f"{f.__name__}.wav", salida, self.rate)
+                        except Exception as e:
+                            print(f"Error al guardar el archivo: {e}")
+                            print(type(salida))
 
                 return salida
 
@@ -69,18 +84,29 @@ class AudioAugmentation:
     @middleware_in(atributo1="loudness_f")
     def loudness(self):
         aug = naa.LoudnessAug()
-        augmented_data = aug.augment(self.data)
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
+    @middleware_in(atributo1="mask_f")
+    def add_mask(self):
+        aug = naa.MaskAug(sampling_rate=self.rate, mask_with_noise=False)
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
+    @middleware_in(atributo1="pitch_f")
+    def pitch(self, fact=(2,3)):
+        aug = naa.PitchAug(sampling_rate=self.rate, factor=fact)
+        augmented_data = aug.augment(self.data)[0]
         return augmented_data
 
     @middleware_in(atributo1="original")
     def get_original(self):
         return self.data
 
-    def crop(self):
-        crop = naa.CropAug(sampling_rate=self.rate, max_percentage=0.5)
-        augmented_data = crop.augment(self.data)
-        if self.graph:
-            self.plot(augmented_data)
+    @middleware_in(atributo1="crop")
+    def add_crop(self, porcentaje: float = 0.5):
+        crop = naa.CropAug(sampling_rate=self.rate)
+        augmented_data = crop.augment(self.data)[0]
         return augmented_data
 
     @staticmethod
@@ -112,25 +138,15 @@ class AudioAugmentation:
         data_noise = data_noise.astype(type(self.data[0]))  # Cast back to same
         return data_noise
 
+    @middleware_in(atributo1="ruido2")
+    def add_noise2(self):
+        aug = naa.NoiseAug()
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
     @middleware_in(atributo1="shift")
     def shift(self):
         return np.roll(self.data, 1600)
-
-    def manipulate(self, data, shift_max, shift_direction='right'):
-        shift = np.random.randint(self.rate * shift_max)
-        if shift_direction == 'right':
-            shift = -shift
-        elif shift_direction == 'both':
-            direction = np.random.randint(0, 2)
-            if direction == 1:
-                shift = -shift
-        augmented_data = np.roll(data, shift)
-        # Set to silence for heading/ tailing
-        if shift > 0:
-            augmented_data[:shift] = 0
-        else:
-            augmented_data[shift:] = 0
-        return augmented_data
 
     @middleware_in(atributo1="stretch")
     def stretch(self, rate_stretch=1.0):
@@ -142,13 +158,59 @@ class AudioAugmentation:
             data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
         return data
 
+    @middleware_in(atributo1="speed")
+    def speed(self):
+        aug = naa.SpeedAug()
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
+    @middleware_in(atributo1="normalizador")
+    def normalizer(self):
+        aug = naa.NormalizeAug(method='minmax')
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
+    @middleware_in(atributo1="polarizador")
+    def polarizer(self):
+        aug = naa.PolarityInverseAug()
+        augmented_data = aug.augment(self.data)[0]
+        return augmented_data
+
+
+class Audio_K(AudioAugmentation):
+
+    def __init__(self, file_path, label: list, save: str = "", grafica: bool = False):
+        super().__init__(file_path, save=save, graph=grafica)
+        self.label = label
+
+    def aumentar(self):
+        all_data = [
+            self.get_original(),
+            self.add_noise(factor_ruido=0.05),
+            self.add_noise2(),
+            self.stretch(rate_stretch=0.8),
+            self.shift(),
+            self.add_crop(),
+            self.loudness(),
+            self.speed(),
+            self.normalizer(),
+            self.polarizer()
+        ]
+
+        all_label = [self.label for _ in range(len(all_data))]
+        return all_data, all_label
+
 
 if __name__ == "__main__":
-    # Create a new instance from AudioAugmentation class
-    aa = AudioAugmentation("data/cat.wav", graph=False, save='data/output/')
-    original = aa.get_original()  # Get original audio
-    data_noise = aa.add_noise(factor_ruido=0.05)  # Adding noise to sound
-    data_stretch = aa.stretch(rate_stretch=0.8)  # Stretching the sound
-    data_roll = aa.shift()  # Shifting the sound
+    data_audio = "data/cat.wav"
+    (data, rate) = librosa.core.load(data_audio)
+    data_audio = (data, rate, "cat")
+    label_audio = [1, 1]
 
-    exit()
+    muestras_wav, instrumentos = Audio_K(data_audio, label=label_audio, save='data/output/').aumentar()
+
+    processAudio = ProcessAudio(rate)
+    for id_audio, dat in enumerate(muestras_wav):
+        processAudio.set_data(dat)
+        caracteristicas = processAudio.get_all(id_audio)  # Extrayendo caracteristicas audios, salen 26 caracteristicas
+    print(len(muestras_wav), len(instrumentos))
